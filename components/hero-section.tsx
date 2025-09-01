aa3e4921e4a8bd8aed146b006cedeb88b2b51203
Sriram MK <mksriram@umich.edu>
2025-09-01 13:26:33 +0300

    added faq

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -17,6 +17,14 @@ export default function HeroSection() {
           >
             Apply now
           </a>
+          {" ("}
+          <a
+            href="https://v1team.notion.site/Product-Studio-FAQ-20e82307c07d80e78cf4e6aac07a1754"
+            className="underline hover:text-yellow-800 transition-colors"
+          >
+            FAQ
+          </a>
+          {")"}
         </span>
       </div>
 
