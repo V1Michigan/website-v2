03e125ec2d264e657dfc08a1f960743b97eda0fe
daniel <78173152+dllliu@users.noreply.github.com>
2026-01-30 17:14:41 -0500

    3 cols (#36)

--- components/what-we-do-section.tsx
+++ components/what-we-do-section.tsx
@@ -2,7 +2,7 @@ import ProgramCard from "./program-card";
 
 export default function WhatWeDoSection() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
       <ProgramCard
         title="Product Studio"
         description="Build something you're passionate about in 8 weeks."
