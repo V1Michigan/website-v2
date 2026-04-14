6f0850e2ba024143d59060cc9d4cc6d60bc16124
Kieran Llarena <baymax709@gmail.com>
2026-04-13 22:27:00 -0400

    feat: del north-star stuff

--- src/app/north-star/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type React from "react";
-
-export default function NorthStarLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-dvh w-screen bg-black text-white font-instrument">{children}</div>;
-}
-
-
